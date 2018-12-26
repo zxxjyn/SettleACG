5a7c94ccd0d3731d6ed0408a20e1468c4e497826
SettleACG <zxxjyn@126.com>
2018-12-26 15:19:52 +0800

    change

--- MY_SPRING_BOOT_1/src/main/java/com/amadusesa/test/Test.java
+++
@@ -1,8 +0,0 @@
-package com.amadusesa.test;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Hello world!!");
-        System.out.println("lalals");
-    }
-}
